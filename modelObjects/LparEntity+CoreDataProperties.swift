a18c70261fc2b83680f8f97d4f5047f085bedfd2
jake berberich <jake.berberich@me.com>
2017-12-16 12:25:03 -0500

    changes to data base layout and import of new version of lparEntity

--- modelObjects/LparEntity+CoreDataProperties.swift
+++ modelObjects/LparEntity+CoreDataProperties.swift
@@ -31,10 +31,14 @@ extension LparEntity {
     @NSManaged public var model: String
     @NSManaged public var numberProcessors: String
     @NSManaged public var osRelease: String
+    @NSManaged public var partionID: String
     @NSManaged public var processorFeature: String
     @NSManaged public var serialNumber: String
     @NSManaged public var serverName: String
+    @NSManaged public var serverPartitions: String
+    @NSManaged public var serverProcessors: String
     @NSManaged public var systemAsp: String
     @NSManaged public var systemType: String
+    @NSManaged public var pgroup: String
 
 }
